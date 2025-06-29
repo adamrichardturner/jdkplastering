94b17cb7ef756c112a506e5c2b7892fb955ef2fe
Adam Richard Turner <hello@adamrichardturner.dev>
2025-06-29 19:41:35 +0100

    hero fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -173,7 +173,7 @@ export default function Index() {
             animate="animate"
           >
             <motion.h1
-              className="text-4xl md:text-5xl lg:text-6xl font-bold mb-8 md:leading-[62px] leading-[42px]"
+              className="text-3xl leading-tight md:text-5xl lg:text-6xl font-bold mb-8 md:leading-[62px] leading-[42px]"
               variants={fadeInUp}
             >
               <span className="text-white">London&apos;s Leading </span>
