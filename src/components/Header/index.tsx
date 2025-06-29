32ded778242e104ac102735fa222176ed417de63
Adam Richard Turner <hello@adamrichardturner.dev>
2025-06-29 01:05:32 +0100

    tighten mobile nav

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -228,7 +228,7 @@ export default function Header({ isMenuOpen, setIsMenuOpen }: HeaderProps) {
 
               {/* Call to Action Section */}
               <motion.div
-                className="mt-16 space-y-3"
+                className="mt-6 space-y-3"
                 initial={{ y: 30, opacity: 0 }}
                 animate={{ y: 0, opacity: 1 }}
                 transition={{ delay: 0.7, duration: 0.6, ease: 'easeOut' }}
